67b57ab7ac62d544883501b815a02a9232316bfd
NachoK00l <zeno77@outlook.be>
2025-01-20 21:01:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from PIL import Image, ImageChops, ImageEnhance, ImageOps
 import edgeDetection
 import effects
 
-image = Image.open("C:/Users/Ohioman/Pictures/image.png")
+image = Image.open("image.png")
 
 edge = edgeDetection.sobelEdgeDetection(image)
 
 edge.show()
 
-melt = effects.melt(image, edge, Length=30)
-newImage = effects.hmelt(melt, edge, Length=30)
+newImage = effects.melt(image, edge, Length=30)
 newImage.show()
 newImage.save('output.png')
